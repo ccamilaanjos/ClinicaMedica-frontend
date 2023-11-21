6f667078028cb9f6d0d941ec30a74c45cbd99e37
Camila Anjos <anjoscam.irl@gmail.com>
2023-11-21 01:57:41 -0300

    Tela de pacientes visualmente pronta.

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -15,19 +15,19 @@ function Homepage() {
 function Body() {
   return (
     <div className='homepage-body'>
-      <h1 className='title'>
+      <h1 className='homepage-title'>
         Painel de Controle
       </h1>
       <div className="button-wrapper">
-        <Link to={'/pacientes'} className='button'>
+        <Link to={'/pacientes'} className='homepage-button'>
           <img src="src\assets\pacientes.png" alt="Pacientes" />
           <span>Pacientes</span>
         </Link>
-        <Link to={'/medicos'} className='button'>
+        <Link to={'/medicos'} className='homepage-button'>
           <img src="src\assets\medicos.png" alt="Médicos" />
           <span>Médicos</span>
         </Link>
-        <Link to={'/consultas'} className='button'>
+        <Link to={'/consultas'} className='homepage-button'>
           <img src="src\assets\consultas.png" alt="Consultas" />
           <span>Consultas</span>
         </Link>
--- src/components/pacientes/index.jsx
+++ src/components/pacientes/index.jsx
@@ -3,18 +3,65 @@ import Header from '../header/index.jsx'
 import './style.css';
 
 function Pacientes() {
-  return(
+  return (
     <div>
-        <Header/>
-        <Body/>
+      <Header />
+      <Body />
     </div>
   )
 }
 
 function Body() {
-    return(
-        <h1>Pacientes</h1>
-    )
-}
+  const pacientes = [
+    { nome: 'Maria', email: 'maria@gmail.com', cpf: '12345678909' },
+    { nome: 'João', email: 'joao@gmail.com', cpf: '98765432101' },
+    { nome: 'Ana', email: 'ana@gmail.com', cpf: '12345678908' },
+    { nome: 'Pedro', email: 'pedro@gmail.com', cpf: '98765432102' },
+    { nome: 'Raul', email: 'raul@gmail.com', cpf: '12345678907' },
+    { nome: 'Lua', email: 'lua@gmail.com', cpf: '98765432103' },
+    { nome: 'Lucas Macedo Rabelo', email: 'lucasrabelo@gmail.com', cpf: '12345678906' },
+    { nome: 'Maria', email: 'maria@gmail.com', cpf: '12345678900' },
+    { nome: 'João', email: 'joao@gmail.com', cpf: '98765432104' },
+    { nome: 'Ana', email: 'ana@gmail.com', cpf: '12345678905' },
+  ];
 
+  return (
+    <div className='container'>
+      <h1 className='pacientes-title'>Pacientes</h1>
+      <div>
+        <div className='cad'>
+          <button className='cadastrar' type='button' onClick={() => { alert(`Cadastrar novo paciente`) }}>Cadastrar novo paciente</button>
+        </div>
+        <div className='pacientes-table'>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pacientes.map(paciente => (
+                <tr key={paciente.cpf}>
+                  <td>{paciente.nome}</td>
+                  <td>{paciente.email}</td>
+                  <td>{paciente.cpf}</td>
+                  <td>
+                    <button className='update' type="button" onClick={() => alert(`Editar paciente: ${paciente.nome}`)}>Atualizar</button>
+                  </td>
+                  <td>
+                    <button className='delete' type="button" onClick={() => alert(`Excluir paciente: ${paciente.nome}`)}>❌</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className='empty'>
+      </div>
+    </div>
+  )
+}
 export default Pacientes;
