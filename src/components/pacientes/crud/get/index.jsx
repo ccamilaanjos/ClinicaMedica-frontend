aa092e378185311c9f020453943ae59389e5cb6a
Camila Anjos <anjoscam.irl@gmail.com>
2023-12-02 00:37:26 -0300

    Tela de pacientes consumindo dados do backend

--- src/components/pacientes/crud/get/index.jsx
+++ src/components/pacientes/crud/get/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../../../api/api'
 
+// Tabela com todos os pacientes ativos
 function getData() {
-    let pacientes = fetchPacientes();
-    
+
+  const [pacientes, setPaciente] = useState([])
+
+  useEffect(() => {
+    let url = 'paciente-ms/pacientes/ativos?page=0';
+
+    API.get(url).then((response) => {
+      setPaciente(response.data["content"]);
+    })
+  }, []);
+
+  function pacienteRow() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>CPF</th>
-          </tr>
-        </thead>
+      <>
         <tbody>
           {pacientes.map(paciente => (
             <tr key={paciente.cpf}>
@@ -26,25 +31,22 @@ function getData() {
               </td>
             </tr>))}
         </tbody>
-      </table>
+      </>
     )
-}
+  }
 
-function fetchPacientes() {
-  const pacientes = [
-    { nome: 'Maria', email: 'maria@gmail.com', cpf: '12345678909' },
-    { nome: 'João', email: 'joao@gmail.com', cpf: '98765432101' },
-    { nome: 'Ana', email: 'ana@gmail.com', cpf: '12345678908' },
-    { nome: 'Pedro', email: 'pedro@gmail.com', cpf: '98765432102' },
-    { nome: 'Raul', email: 'raul@gmail.com', cpf: '12345678907' },
-    { nome: 'Lua', email: 'lua@gmail.com', cpf: '98765432103' },
-    { nome: 'Lucas Macedo Rabelo', email: 'lucasrabelo@gmail.com', cpf: '12345678906' },
-    { nome: 'Maria', email: 'maria@gmail.com', cpf: '12345678900' },
-    { nome: 'João', email: 'joao@gmail.com', cpf: '98765432104' },
-    { nome: 'Ana', email: 'ana@gmail.com', cpf: '12345678905' },
-  ];
-  
-  return pacientes;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>CPF</th>
+        </tr>
+      </thead>
+      {pacienteRow(pacientes)}
+    </table>
+  )
 }
 
 export default getData;
